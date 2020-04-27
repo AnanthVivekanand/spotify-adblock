e8f171ecc34601c57f3e244abd0cd70f6fcdb753
AnanthVivekanand <avivek3021@gmail.com>
2020-04-27 12:22:11 -0500

    Basic mitm proxy

---
+++ mitm2.js
@@ -0,0 +1,101 @@
+var mitm_Proxy = require('http-mitm-proxy');
+var mitm_proxy = new mitm_Proxy();
+var path = require('path');
+var CA = require('./CA.js');
+var net = require('net');
+const micromatch = require('micromatch');
+const whitelist = require('./whitelist-mitm.js');
+const blacklist = require('./blacklist.js');
+
+mitm_proxy.onError(function (ctx, err) {
+    if (err.code === "ERR_SSL_SSLV3_ALERT_CERTIFICATE_UNKNOWN") {
+	console.log("You haven't installed the generated CA certificate");
+	process.exit(1);
+    }
+});
+
+mitm_proxy.onConnect(function(req, socket, head, callback) {
+  var host = req.url.split(":")[0];
+  var port = req.url.split(":")[1];
+
+  // not the most elegant, but since 
+  // spclient.wg.spotify.com is the only url
+  // that we need to filter the complete url by
+  // we'll just use a simple if statement for now
+  if (host == "spclient.wg.spotify.com") {
+    return callback();
+  }
+
+  // is our host in the whitelist?
+  // proxy it through, no questions asked
+  else if (micromatch.isMatch(host, whitelist)) {
+    console.log("Allowing: " + host);
+    var conn = net.connect({
+      port: port,
+      host: host,
+      allowHalfOpen: true
+    }, function () {
+      conn.on('finish', () => {
+        socket.destroy();
+      });
+      socket.on('close', () => {
+        conn.end();
+      });
+      socket.write('HTTP/1.1 200 OK\r\n\r\n', 'UTF-8', function () {
+        conn.pipe(socket);
+        socket.pipe(conn);
+      })
+    });
+  } 
+
+  // okay, we have no idea what this is, so
+  // we'll just block this
+  else {
+    console.log("Blocking: " + host);
+  }
+});
+
+mitm_proxy.onRequest(function(ctx, callback) {
+  // we now have complete access to the url
+  let completeUrl = "https://" + ctx.clientToProxyRequest.headers.host + ctx.clientToProxyRequest.url;
+
+  if (micromatch.isMatch(completeUrl, blacklist)) {
+    console.log("Blocked: " + completeUrl);
+    ctx.proxyToClientResponse.end(''); // terminate it
+  } else {
+    return callback();
+  }
+});
+
+mitm_proxy.onCertificateRequired = function(hostname, callback) {
+  return callback(null, {
+    keyFile: path.resolve('./certs/', hostname + '.key'),
+    certFile: path.resolve('./certs/', hostname + '.crt')
+  });
+};
+
+mitm_proxy.onCertificateMissing = function (ctx, files, callback) {
+  var hosts = files.hosts || [ctx.hostname];
+  CA.generateServerCertificateKeys(hosts, function (cert, private) {
+    callback(null, {
+      certFileData: cert,
+      keyFileData: private,
+      hosts: hosts
+    });
+  });
+  return this;
+};
+
+mitm_proxy.start = async function(opt) {
+  await CA.create(path.resolve(process.cwd(), 'certs'), function (err, ca) {
+    if (err) {
+      console.log(err);
+      return callback(err);
+    }
+    CA = ca;
+  });
+  mitm_proxy.listen(opt)
+  console.log("Proxy is up on port " + opt.port);
+};
+
+mitm_proxy.start({port: 8082});
