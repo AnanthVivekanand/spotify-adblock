c9d947a17b414895ec2aab098848d347a17bcb6a
AnanthVivekanand <avivek3021@gmail.com>
2021-05-07 23:52:51 -0500

    Updated MITM whitelist
    From https://github.com/abba23/spotify-adblock-linux/blob/master/whitelist.h

--- mitm-utils/whitelist-mitm.js
+++ mitm-utils/whitelist-mitm.js
@@ -1,5 +1,5 @@
 const whitelist = [
-  "localhost", // for using local proxies
+  "localhost", // local proxies
   "audio-sp-*.pscdn.co", // audio
   "audio-fa.scdn.co", // audio
   "audio4-fa.scdn.co", // audio
@@ -10,6 +10,7 @@ const whitelist = [
   "i.scdn.co", // cover art
   "lineup-images.scdn.co", // playlists lineup images
   "merch-img.scdn.co", // merch images
+  "misc.scdn.co", // miscellaneous images
   "mosaic.scdn.co", // playlist mosaic images
   "newjams-images.scdn.co", // release radar images
   "o.scdn.co", // cover art
@@ -21,23 +22,43 @@ const whitelist = [
   "video-fa.scdn.co", // videos
   "content.production.cdn.art19.com", // podcasts
   "rss.art19.com", // podcasts
+  "*.buzzsprout.com", // podcasts
+  "platform-lookaside.fbsbx.com", // Facebook profile images
+  "genius.com", // lyrics (genius-spicetify)
+  "*.gvt1.com", // Widevine download
   "hwcdn.libsyn.com", // podcasts
   "traffic.libsyn.com", // podcasts
+  "api*-desktop.musixmatch.com", // lyrics (genius-spicetify)
+  "*.podbean.com", // podcasts
+  "dts.podtrac.com", // podcasts
   "www.podtrac.com", // podcasts
   "audio.simplecast.com", // podcasts
   "media.simplecast.com", // podcasts
   "ap.spotify.com", // audio (access point)
   "*.ap.spotify.com", // resolved access points
+  "api.spotify.com", // client APIs
+  "api-partner.spotify.com", // album/artist pages
+  "xpui.app.spotify.com", // user interface
   "apresolve.spotify.com", // access point resolving
-  "dealer.spotify.com", // discord rich presence
-  "image-upload.spotify.com", // image uploading
+  "clienttoken.spotify.com", // login
+  "*dealer.spotify.com", // websocket connections
+  "image-upload*.spotify.com", // image uploading
   "login*.spotify.com", // login
-  "spclient.wg.spotify.com", // ads/tracking (blocked in blacklist), radio, recently played, friend activity,...
+  "*-spclient.spotify.com", // client APIs
+  "spclient.wg.spotify.com", // client APIs, ads/tracking (blocked in blacklist)
+  "audio-fa.spotifycdn.com", // audio
+  "seed-mix-image.spotifycdn.com", // mix images
+  "download.ted.com", // podcasts
   "dcs*.megaphone.fm", // podcasts
   "traffic.megaphone.fm", // podcasts
+  "audio-ak-spotify-com.akamaized.net", // audio
+  "audio4-ak-spotify-com.akamaized.net", // audio
   "heads4-ak-spotify-com.akamaized.net", // audio (heads)
   "audio4-ak.spotify.com.edgesuite.net", // audio
+  "scontent*.fbcdn.net", // Facebook profile images
   "audio-sp-*.spotifycdn.net", // audio
+  "dovetail.prxu.org", // podcasts
+  "dovetail-cdn.prxu.org" // podcasts
 ];
 
 module.exports = whitelist;
